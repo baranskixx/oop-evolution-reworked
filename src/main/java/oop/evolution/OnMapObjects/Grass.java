28d5b070776c65f58171ea2f0686b84e5cfd96bd
Adam Baranski <xxbaranski@gmail.com>
2022-02-18 13:52:06 +0100

    Implemented Grass.java class.

--- src/main/java/oop/evolution/OnMapObjects/Grass.java
+++ src/main/java/oop/evolution/OnMapObjects/Grass.java
@@ -3,14 +3,43 @@ package oop.evolution.OnMapObjects;
 import oop.evolution.Interfaces.IMapElement;
 import oop.evolution.Vector2d;
 
+import java.util.Objects;
+
 /**
  * Class representing single field of grass on the map.
  *
  * @author baranskia
  */
 public class Grass implements IMapElement {
+    public final Vector2d position;
+
+    /**
+     * Default constructor.
+     * @param pos Position where grass is placed on.
+     */
+    public Grass(Vector2d pos){
+        position = pos;
+    }
+
+    /**
+     * Position getter.
+     * @return Position where grass is placed.
+     */
     @Override
     public Vector2d getPosition() {
-        return null;
+        return position;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Grass grass = (Grass) o;
+        return position.equals(grass.position);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(position);
     }
 }
